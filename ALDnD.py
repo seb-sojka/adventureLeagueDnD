a78a17a18e2c9b56c9a7023fe3e3f7e8097cbaee
seb-sojka <sojkas@oregonstate.edu>
2016-06-05 17:56:36 -0500

    Add python work

---
+++ ALDnD.py
@@ -0,0 +1,7 @@
+#Leads user to create a level 1 character for adventurer league play
+import csv
+import Tkinter
+abilityScores = [8,10,12,13,14,15]#The value for ablity scores that are possible
+conScore = 10
+strScore = 10
+strScore = 10
