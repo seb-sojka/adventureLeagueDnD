b670c6f31b748eeaf5cb075421cea06d20d15c0d
seb-sojka <sojkas@oregonstate.edu>
2016-06-12 14:56:34 -0500

    Now calculates ability score for player race

--- ALDnD.py
+++ ALDnD.py
@@ -1,5 +1,6 @@
 #Leads user to create a level 1 character for adventurer league play
-#import csv
+import csv
+import numpy as np
 #import Tkinter
 abilityScores = [8,10,12,13,14,15]#The value for ablity scores that are possible
 conScore = abilityScores[0]
@@ -8,3 +9,21 @@ dexScore = abilityScores[2]
 intScore = abilityScores[3]
 wisScore = abilityScores[4]
 chaScore = abilityScores[5]
+playerRace = "Normal Human"
+playerClass = "Fighter"
+with open("Races.csv") as csvRace:
+    reader = csv.DictReader(csvRace)
+    data ={}
+    for row in reader:
+        for header, value in row.items():
+            try:
+                data[header].append(value)
+            except KeyError:
+                data[header]=[value]
+raceNumber = data['Race'].index(playerRace)
+strScore= strScore+int(data['Strength'][raceNumber])
+conScore= conScore+int(data['Consitution'][raceNumber])
+dexScore= dexScore+int(data['Dexterity'][raceNumber])
+intScore= intScore+int(data['Intelligence'][raceNumber])
+wisScore = wisScore+int(data['Wisdom'][raceNumber])
+chaScore = chaScore +int(data['Charisma'][raceNumber])
